6f83e832d18c6450937cc4315d897a543d7ca7c7
Melvin <domingo.jm029@gmail.com>
2026-03-15 13:22:03 +0800

    fix: client.ts

--- client/src/lib/client.ts
+++ client/src/lib/client.ts
@@ -2,7 +2,7 @@ import { hc } from "hono/client";
 import type { AppType } from "../../../backend/src/types/hono";
 // import type { AppType } from "../../../backend/src/types/hono";
 
-export const client = hc<AppType>("/", {
+export const client = hc<AppType>("https://vin-todo.up.railway.app", {
 	init: {
 		credentials: "include",
 		headers: {
