48e33a9e0e8b57a1cebc2631bf90469c01507db3
Melvin <domingo.jm029@gmail.com>
2026-03-16 14:02:14 +0800

    fix: authClient.ts

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 console.log(import.meta.env.VITE_API_URL);
 export const authClient = createAuthClient({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: import.meta.env.VITE_API_URL || "http://localhost:4001",
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
