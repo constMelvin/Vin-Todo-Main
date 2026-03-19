2a42b0a36831633556fe82f5f262d5e2ea32aae6
Melvin <domingo.jm029@gmail.com>
2026-03-19 23:27:17 +0800

    fix: fixed error-handler

--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -1,18 +1,6 @@
-// import type { HonoEnv } from '@/types/hono';
-// import { pinoLogger } from '@/utils/logger';
 import { makeError } from "utils/errors.js";
 import type { Context } from "hono";
-
-
 export async function errorHandlerMiddleware(err: Error, c: Context) {
 	const { error, statusCode } = makeError(err);
-
-	// pinoLogger.error(error.message, error);
-	// const errorContextData = {
-	//   context: JSON.stringify(c, null, 2),
-	//   statusCode: statusCode,
-	//   error: error,
-	// };
-	// sentryClient.captureException(err, { contexts: { data: errorContextData } });
 	return c.json(error, { status: statusCode } as any);
 }
