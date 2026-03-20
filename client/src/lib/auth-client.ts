7e6e2404cf7caefd4f2ea0e28d938f928d055420
Melvin <domingo.jm029@gmail.com>
2026-03-20 11:04:39 +0800

    fix: auth-client.ts

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react";
 
 console.log(import.meta.env.VITE_API_URL);
 export const authClient = createAuthClient({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: "https://vin-todo.vercel.app",
+	// baseURL: import.meta.env.VITE_API_URL,
 	fetchOptions: {
 		credentials: "include",
 		onSuccess: (ctx) => {
